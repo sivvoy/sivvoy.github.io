1c426840f07b1c6be1f2bc8276b22b26b517e5cb
Sivvoy Taing <sivvo@yahoo.com>
2019-03-13 21:24:51 +0000

    My first commit

---
+++ precache-manifest.f85852f12afc7b1eead660d7b44b2564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "57346729467cf7396a08",
+    "url": "/static/js/main.57346729.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "57346729467cf7396a08",
+    "url": "/static/css/main.d78c728c.chunk.css"
+  },
+  {
+    "revision": "c2cbe82c71435bce80f7b3b3278acf70",
+    "url": "/index.html"
+  }
+];
